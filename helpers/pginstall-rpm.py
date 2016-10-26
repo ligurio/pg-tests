c78549ba6639efa065cbe403cf95555761aeac4a
Sergey Bronnikov <sergeyb@bronevichok.ru>
2016-10-26 12:41:59 +0300

    Initial version of PostgreSQL test system
    This commit brings several things:
    - script test-run.py prepare test env and run command inside test env
    - preparation of environment is done using Ansible playbook
    - script pginstall-rpm.py installs product before testing

---
+++ helpers/pginstall-rpm.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+import fileinput
+import os
+import platform
+import re
+import shutil
+import subprocess
+import sys
+from subprocess import Popen, PIPE, STDOUT
+import urllib2
+
+rpm = [ 'server', 'contrib', 'perl', 'python', 'tcl', 'devel', 'docs', 'docs-ru' ]
+deb = [ 'server', 'contrib', 'plperl', 'plpython', 'pltcl', 'libs', 'devel', 'docs', 'docs-ru' ]
+
+repohost = "http://repo.postgrespro.ru"
+
+dist = { 'oracle': "Oracle Linux Server", 		\
+         'centos': "CentOS Linux",         		\
+         'centos6': "CentOS",               		\
+         'rhel': "RHEL",                   		\
+         'rhel7': "Red Hat Enterprise Linux Server",   	\
+         'debian': "debian",               		\
+         'ubuntu': "Ubuntu",               		\
+         'sles': "SLES",                   		\
+         'alt-centaur': "AltLinux",       		\
+         'alt-spt6': "AltLinux",           		\
+         'alt-spt7': "AltLinux",           		\
+         'rosa': "ROSA",                   		\
+         'rosa-sx': "ROSA SX",             		\
+         'rosa-dx': "ROSA DX",             		\
+         'rosa-marathon': "ROSA Marathon", 		\
+         'astra': "Astra Linux" }
+
+rpm_based = [dist['centos'], dist['rhel'], dist['centos6'], dist['rhel7'], dist['oracle']]
+deb_based = [dist['debian'], dist['ubuntu']]
+
+def get_distro():
+    """
+    Detect OS family and version
+    """
+
+    os = {
+        'type': platform.system(),
+        'distro': platform.linux_distribution()[0],
+        'version': platform.linux_distribution()[1].split(".")[0],
+        'arch': platform.architecture()[0]
+    }	
+    
+    return os
+
+def product_name(name, edition, major, minor, milestone):
+
+    product_dir = "pgpro-%s.%s" % (major, minor)
+    if edition == "ee":
+       product_dir = "pgproee-%s.%s" % (major, minor)
+    if milestone:
+       product_dir = product_dir + "-" + milestone
+
+    return product_dir
+
+def get_gpg_key():
+
+    keyurl = "%s/%s/keys/GPG-KEY-POSTGRESPRO-%s%s" % \
+	(repohost, product_dir, major, minor)
+    print keyurl
+
+    response= urllib2.urlopen(keyurl)
+    key = response.read()
+
+    filename = keyurl.rsplit('/', 1)[1]
+    with open(filename, "a") as f:
+         f.write(key)
+
+    return filename
+
+def setup_repo(d, major, minor, name, edition, milestone, build):
+
+    product_dir = product_name(name, edition, major, minor, milestone)
+
+    print "Install PostgreSQL repo package"
+
+    if d['distro'] == dist['centos'] or d['distro'] == dist['centos6']:
+       base_url = os.path.join(product_dir, "centos", d['version'], "os/x86_64/rpms/")
+       repo_package = "postgrespro-%s.%s.centos.pro.yum-%s.%s-0.%s.noarch.rpm" % (major, minor, major, minor, build)
+       repo_file = "/etc/yum.repos.d/postgrespro-%s.%s.centos.pro.repo" % (major, minor)
+    elif d['distro'] == dist['oracle']:
+       base_url = os.path.join(product_dir, "oraclelinux", d['version'] + "Server", "os/x86_64/rpms/")
+       repo_package = "postgrespro-%s.%s.oraclelinux.pro.yum-%s.%s-0.%s.noarch.rpm" % (major, minor, major, minor, build)
+       repo_file = "/etc/yum.repos.d/postgrespro-%s.%s.oraclelinux.pro.repo" % (major, minor)
+    elif d['distro'] == dist['rhel'] or d['distro'] == dist['rhel7']:
+       base_url = os.path.join(product_dir, "rhel", d['version'] + "Server", "os/x86_64/rpms/")
+       repo_package = "postgrespro-%s.%s.rhel.pro.yum-%s.%s-0.%s.noarch.rpm" % (major, minor, major, minor, build)
+       repo_file = "/etc/yum.repos.d/postgrespro-%s.%s.rhel.pro.repo" % (major, minor)
+    else:
+       print "Unsupported distro ", d
+       sys.exit(1)
+
+    pkg_url = os.path.join(repohost, base_url, repo_package)
+    print "URL to the package with repository", pkg_url
+    retcode = subprocess.call(["rpm", "-ihv", pkg_url])
+    if retcode:
+       print "Installation of %s failed" % pkg_url
+       sys.exit(1)
+
+    fix_version(major, minor, milestone, edition, repo_file)
+
+def package_mgmt(major, minor, milestone, edition, distro, action):
+
+    pkg_mask = "postgrespro%s%s-*" % (major, minor)
+    if edition == "ee":
+       pkg_mask = "postgrespro-enterprise%s%s-*" % (major, minor)
+    
+    if distro in rpm_based:
+       if action == "install":
+          print "Install PostgreSQL packages with mask", pkg_mask
+          subprocess.call(["yum", "install", "-y", pkg_mask])
+       elif action == "remove":
+          print "Remove PostgreSQL packages with mask", pkg_mask
+          subprocess.call(["service", "postgresql-%s.%s" % (major, minor), "stop"])
+          subprocess.call(["yum", "remove", "-y", pkg_mask])
+          subprocess.call(["yum", "remove", "-y", "postgrespro-%s.%s" % (major, minor)])
+          # FIXME: remove package with repo taken from setup_repo()
+
+def setup_psql(major, minor, distro):
+
+    print "Setup PostgreSQL service"
+    if distro in rpm_based:
+       subprocess.call(["service", "postgresql-%s.%s" %  (major, minor), "initdb"])
+       subprocess.call(["service", "postgresql-%s.%s" % (major, minor), "start"])
+       subprocess.call(["chkconfig", "postgresql-%s.%s" % (major, minor), "on"])
+
+    path = os.environ['PATH']
+    path = path + "/usr/pgsql-%s.%s/bin/" % (major, minor)
+    print os.environ['PATH']
+
+    PG_PASSWORD = 'password'
+    subprocess.call(["sudo", "-u", "postgres", "psql", "-c", "ALTER USER postgres WITH PASSWORD '%s';" % PG_PASSWORD])
+
+    hba_auth = """
+local   all             all                                     peer
+host    all             all             0.0.0.0/0               trust
+host    all             all             ::0/0                   trust"""
+
+    #subprocess.call(["sudo", "-u", "postgres", "psql", "format=unaligned", "-c", "SHOW hba_file;"])
+    hba_file = "/var/lib/pgsql/%s.%s/data/pg_hba.conf" % (major, minor)
+
+    hba = fileinput.FileInput(hba_file, inplace=True)
+    for line in hba:
+        if line[0] <> '#':
+           line = re.sub('^', '#', line.rstrip())
+        print line.rstrip()
+
+    with open(hba_file, 'a') as hba:
+         hba.write(hba_auth)
+    subprocess.call(["chown", "postgres:postgres", hba_file])
+
+    subprocess.call(["sudo", "-u", "postgres", "psql", "-c", "ALTER SYSTEM SET listen_addresses to '*';"])
+    subprocess.call(["service", "postgresql-%s.%s" % (major, minor), "restart"])
+
+def fix_version(major, minor, milestone, edition, repo_file):
+
+    if milestone:
+       print "Fix product version in", repo_file
+       product = "pgpro"
+       if edition == "ee":
+          product = "pgproee"
+       find = product + "-" + major + "." + minor
+       replace = find + "-" + milestone + "/"
+       find = product + "-" + major + "." + minor + "/"
+
+       repo = fileinput.FileInput(repo_file, inplace=True)
+       for line in repo:
+           line = re.sub(find, replace, line.rstrip())
+           print(line)
+
+def install_product(name, edition, version, milestone, build):
+
+    major = version.split(".")[0]
+    minor = version.split(".")[1]
+
+    os.environ['PATH'] = os.environ['PATH'] + "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/root/bin"
+    d = get_distro()
+    setup_repo(d, major, minor, name, edition, milestone, build)
+    package_mgmt(major, minor, milestone, edition, d['distro'], "install")
+    setup_psql(major, minor, d['distro'])
+
+if __name__ == '__main__':
+
+    version = "9.6"
+    milestone = "beta"
+    name = "postgrespro"
+    edition = "opensource"
+    build = "1"
+
+    print "PARAMETERS:", name, edition, version, milestone
+    install_product(name, edition, version, milestone, build)
---
+++ test-run.py
@@ -0,0 +1,218 @@
+#!/bin/env python
+
+"""PostgresPro regression tests run script."""
+
+__author__ = "Sergey Bronnikov <sergeyb@postgrespro.ru>"
+
+import argparse
+import json
+import libvirt
+import logging
+import os
+import os.path
+import paramiko
+import random
+import re
+import socket
+import shutil
+import sys
+import time
+import urllib
+from subprocess import call
+
+IMAGE_BASE_URL = 'http://webdav.l.postgrespro.ru/DIST/vm-images/test/'
+TEMPLATE_DIR = '/home/sergeyb/original_vms/'
+WORK_DIR = '/vz/working_vms/'
+ANSIBLE_PLAYBOOK = 'static/playbook-prepare-env.yml'
+
+SSH_LOGIN = 'test'
+SSH_PASSWORD = 'TestPass1'
+SSH_ROOT_PASSWORD = 'TestRoot1'
+SSH_PORT = 22
+DEBUG = False
+
+def list_images():
+   names = []
+   page = urllib.urlopen(IMAGE_BASE_URL).read()
+   images = re.findall('href=[\'"]?([^\'" >]+)\.qcow2', page)
+   for i in images:
+       names.append(i)
+   return names
+
+def lookupIPbyMac(conn, mac):
+    """Lookup IP adddress by MAC address using DHCP information
+
+    :parameters: connection object, mac address
+    :return: string: IP address
+    """
+    for net in conn.listNetworks():
+        for lease in conn.networkLookupByName(net).DHCPLeases():
+            if lease['mac'] == mac and lease['iaid'] == None:
+               return lease['ipaddr']
+    return None
+
+# TODO: use Locally Administered Address Ranges
+def mac_address_generator():
+    """Generate random mac address
+
+    :return: string: mac address
+    """
+    mac = [0x00, 0x16, 0x3e,
+            random.randint(0x00, 0x7f),
+            random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff)]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+
+def exec_command(cmd, hostname):
+
+    known_hosts = os.path.expanduser(os.path.join("~", ".ssh", "known_hosts"))
+    try:
+       client = paramiko.SSHClient()
+       if os.path.isfile(known_hosts):
+          client.load_system_host_keys(known_hosts)
+       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+       client.connect(hostname=hostname, username=SSH_LOGIN, \
+		      password=SSH_PASSWORD, port=SSH_PORT, look_for_keys=False)
+    except paramiko.AuthenticationException, e:
+       print 'Auth Error: ', e
+       sys.exit(1)
+    except paramiko.SSHException, e:
+       print 'Connection Error: ', e
+       sys.exit(1)
+    except socket.error, e:
+       print 'Timed Out', e
+       sys.exit(1)
+
+    stdin, stdout, stderr = client.exec_command(cmd)
+    print stdout.read() + stderr.read()
+    client.close()
+
+
+#######################################################################
+# Program body
+#######################################################################
+
+def main():
+
+    names = list_images()
+    parser = argparse.ArgumentParser(description='PostgreSQL regression tests run script.')
+    parser.add_argument('--target', dest = "target", choices=names, \
+		        help='System under test (image)')
+    parser.add_argument('--test', dest = "run_tests", help='Tests to run (default: all)')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+       parser.print_help()
+       sys.exit(0)
+
+    name = None
+    if args.target is not None:
+       name = args.target
+       if args.run_tests is None:
+          tests = '*'
+       tests = args.run_tests
+
+    try:
+       conn = libvirt.open(None)
+    except libvirt.libvirtError, e:
+       print 'LibVirt connect error: ', e
+       sys.exit(1)
+
+    domname = name + '-' + str(random.getrandbits(15))
+    domimage = WORK_DIR + domname + '.qcow2'
+    image_url = IMAGE_BASE_URL + name + '.qcow2'
+    image_original = TEMPLATE_DIR + name + '.qcow2'
+
+    if not os.path.isfile(image_original):
+       if not os.path.exists(TEMPLATE_DIR):
+          os.makedirs(TEMPLATE_DIR)
+       image = urllib.URLopener()
+       image.retrieve(image_url, image_original)
+
+    if not os.path.exists(WORK_DIR):
+       os.makedirs(WORK_DIR)
+    print "Copy an original image to %s" % domimage
+    try:
+        shutil.copy(image_original, domimage)
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+    # FIXME: use linked clone qemu-img create -f qcow2 -b winxp.qcow2 winxp-clone.qcow2
+
+    dommac = mac_address_generator()
+    xmldesc = """<domain type='kvm'>
+  <name>%s</name>
+  <memory unit='GB'>1</memory>
+  <vcpu>1</vcpu>
+  <os>
+    <type>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <features>
+    <acpi/>
+  </features>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/libexec/qemu-kvm</emulator>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2' cache='none'/>
+      <source file='%s'/>
+      <target dev='vda' bus='virtio'/>
+    </disk>
+    <interface type='bridge'>
+      <mac address='%s'/>
+      <source bridge='virbr0'/>
+      <model type='virtio'/>
+    </interface>
+    <input type='tablet' bus='usb'/>
+    <input type='mouse' bus='ps2'/>
+    <graphics type='vnc' port='-1' listen='0.0.0.0'/>
+  </devices>
+</domain>
+""" % (domname, domimage, dommac)
+
+    dom = conn.createLinux(xmldesc, 0)
+    if dom == None:
+       print "Failed to create a test domain"
+
+    domipaddress = None
+    timeout = 0
+    while not domipaddress:
+        timeout = timeout + 5
+        print "Waiting for IP address...%d" % timeout
+        time.sleep(timeout)
+        domipaddress = lookupIPbyMac(conn, dommac)
+        if timeout == 40:
+           print "Failed to obtain an IP address inside domain"
+           sys.exit(1)
+
+    print "Domain name: %s\nIP address: %s" % (dom.name(), domipaddress)
+
+    with open("/etc/hosts", "a") as hosts:
+         hosts.write("{}	{}\n".format(domipaddress, dom.name()))
+
+    with open("static/inventory", "a") as hosts:
+	 inv = "%s ansible_host=%s ansible_become_pass=%s ansible_ssh_pass=%s \
+		ansible_user=%s ansible_become_user=root\n" % \
+		(dom.name(), domipaddress, SSH_ROOT_PASSWORD, SSH_PASSWORD, SSH_LOGIN)
+         hosts.write(inv)
+
+    os.environ['ANSIBLE_HOST_KEY_CHECKING'] = 'False'
+    ansible_cmd = "ansible-playbook %s -i static/inventory -c paramiko --limit %s" % (ANSIBLE_PLAYBOOK, dom.name())
+    if DEBUG:
+       ansible_cmd = ansible_cmd + " -vvv"
+    print ansible_cmd
+    call(ansible_cmd.split(' '))
+
+    cmd = 'pwd'
+    print "Exec %s on %s" % (domipaddress, cmd)
+    exec_command(cmd, domipaddress)
+
+    #dom.destroy()
+
+if __name__ == "__main__":
+    exit(main())
