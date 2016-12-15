7f184a36c4fd492243a8fbf6397b99a64a7350a0
m.samoylov <m.samoylov@postgrespro.ru>
2016-12-15 12:14:46 +0300

    New compression test

--- helpers/sql_helpers.py
+++ helpers/sql_helpers.py
@@ -1,3 +1,10 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+# TODO Change to class  all methods
+
+
 def get_pgpro_info(pgpgro_version):
     """
 
@@ -10,3 +17,28 @@ def get_pgpro_info(pgpgro_version):
     return {'version': version,
             'name': name,
             'build': build}
+
+
+def get_data_directory():
+    """
+    Get data directory
+    :return: string with path to data directory
+    """
+    conn_string = "host='localhost' user='postgres' "
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute('show data_directory')
+    return cursor.fetchall()[0][0]
+
+
+def create_test_database(db_name):
+    """
+    Create database
+    :param db_name: string with db name
+    :return:
+    """
+    conn_string = "host='localhost' user='postgres' "
+    conn = psycopg2.connect(conn_string)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE {}".format(db_name))
