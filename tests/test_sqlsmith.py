e90bc8a3b44175dae2ba6597303a8d5b9eb408ad
Sergey Bronnikov <sergeyb@bronevichok.ru>
2016-12-29 09:56:57 +0300

    Initial version of test with sqlsmith

---
+++ tests/test_sqlsmith.py
@@ -0,0 +1,28 @@
+import platform
+import pytest
+import subprocess
+
+from helpers.pginstall import setup_repo
+
+NUM_QUERIES = 1000000
+
+
+@pytest.fixture(scope="session")
+def install_sqlsmith(request):
+
+    setup_repo("postgresql", "9.6")
+    subprocess.call(["apt-get", "install", "-y", "sqlsmith"])
+    return subprocess.call("type sqlsmith", shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+@pytest.mark.slowtest
+@pytest.mark.skipif('platform.linux_distribution()[0] != "Ubuntu"')
+@pytest.mark.usefixtures('install_postgres')
+@pytest.mark.usefixtures('install_sqlsmith')
+def test_sqlsmith(install_sqlsmith):
+
+    sqlsmith_cmd = ["sudo", "-u", "postgres",
+                    "sqlsmith", "--max-queries=%s" % NUM_QUERIES]
+    sqlsmith = subprocess.Popen(sqlsmith_cmd)
+    assert sqlsmith.wait() == 0
