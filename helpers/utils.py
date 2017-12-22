bdeb4e78295abb003113f9261360ffd9f9f49d42
Alexander Lakhin <a.lakhin@postgrespro.ru>
2017-12-22 18:06:44 +0300

    Wrap long lines, fix formatting (helpers/utils.py)

--- helpers/utils.py
+++ helpers/utils.py
@@ -27,11 +27,14 @@ class MySuites(Enum):
     EPIC = 'epic'
 
 
-def command_executor(cmd, remote=False, host=None, login=None, password=None, stdout=False, windows=False):
-    """ Command executor for local commands and remote commands. For local command using subprocess
-    for remote command paramiko or winrm (for windows)
+def command_executor(cmd, remote=False, host=None,
+                     login=None, password=None,
+                     stdout=False, windows=False):
+    """ Command executor for local commands and remote commands.
+        For local command using subprocess
+        for remote command paramiko or winrm (for windows)
 
-    :param cmd: st: commands with options, separator for each option whitespace
+    :param cmd: commands with options, separator for each option whitespace
     :param remote:  bool, local or remote command
     :param login: str: only for remote command ssh/winrm login
     :param password: str: only for remote command ssh/winrm  password
@@ -46,16 +49,21 @@ def command_executor(cmd, remote=False, host=None, login=None, password=None, st
             first_command = cmd[:cmd.index("|")]
             second_command = cmd[cmd.index("|") + 1:]
             first_command_execute = subprocess.Popen(
-                shlex.split(first_command, posix=not(windows)), stdout=subprocess.PIPE)
-            return subprocess.check_output(shlex.split(second_command, posix=not(windows)),
-                                           stdin=first_command_execute.stdout)
+                shlex.split(first_command, posix=not(windows)),
+                stdout=subprocess.PIPE)
+            return subprocess.check_output(
+                shlex.split(second_command, posix=not(windows)),
+                stdin=first_command_execute.stdout)
         else:
             if stdout:
-                out = subprocess.Popen((shlex.split(cmd, posix=not(windows))), stdout=subprocess.PIPE)
+                out = subprocess.Popen(
+                    shlex.split(cmd, posix=not(windows)),
+                    stdout=subprocess.PIPE)
                 return out.stdout.readline().rstrip()
             else:
                 if windows:
-                    return subprocess.check_call(shlex.split(cmd, posix=False), shell=True)
+                    return subprocess.check_call(
+                        shlex.split(cmd, posix=False), shell=True)
                 else:
                     print(cmd)
                     return subprocess.check_call(shlex.split(cmd))
@@ -66,11 +74,14 @@ def get_virt_ip():
 
     :return: string ip address
     """
-    return subprocess.check_output('ip addr show virbr0', shell=True).split("inet ")[1].split("/")[0]
+    return subprocess.check_output(
+        'ip addr show virbr0', shell=True).split("inet ")[1].split("/")[0]
 
 
-def copy_file(remote_path, local_path, hostname, dir=False, operating_system=None,
-              product_name=None, product_version=None, product_edition=None, tests=None):
+def copy_file(remote_path, local_path, hostname, dir=False,
+              operating_system=None,
+              product_name=None, product_version=None, product_edition=None,
+              tests=None):
     import paramiko
 
     transport = paramiko.Transport((hostname, SSH_PORT))
@@ -79,23 +90,13 @@ def copy_file(remote_path, local_path, hostname, dir=False, operating_system=Non
     if dir:
         print(sftp.listdir(remote_path))
         for file in sftp.listdir(remote_path):
-            print "Copying file '%s', remote host is '%s'" % (file, hostname)
-            sftp.get(os.path.join(remote_path, file), os.path.join(local_path, file))
-            # if '.xml' in file and 'environment' not in file:
-            #     new_file_name = "{}_{}_{}_{}_{}-testsuite.xml".format(operating_system, product_name,
-            #                                                           product_version, product_edition, tests)
-            #     print "Copying file '%s', remote host is '%s'" % (file, hostname)
-            #     sftp.get(os.path.join(remote_path, file), os.path.join(local_path, new_file_name))
-            # elif '.txt' in file:
-            #     print "Copying file '%s', remote host is '%s'" % (file, hostname)
-            #     sftp.get(os.path.join(remote_path, file), os.path.join(local_path, file))
-            # elif '.xml' in file and 'environment' in file:
-            #     print "Copying file '%s', remote host is '%s'" % (file, hostname)
-            #     sftp.get(os.path.join(remote_path, file), os.path.join(local_path, file))
-            # else:
-            #     continue
+            print "Copying file '%s', remote host is '%s'" % (
+                file, hostname)
+            sftp.get(os.path.join(remote_path, file),
+                     os.path.join(local_path, file))
     else:
-        print "Copying file '%s', remote host is '%s'" % (remote_path, hostname)
+        print "Copying file '%s', remote host is '%s'" % (
+            remote_path, hostname)
         sftp.get(remote_path, local_path)
     sftp.close()
     transport.close()
@@ -124,10 +125,12 @@ def copy_reports_win(reportname, reportsdir, destreports, domipaddress):
     cmd = r'xcopy /Y /F .\pg-tests\reports {0}\\{1}\\'.format(
         share, reportsdir.replace('/', '\\'))
     exec_command_win(cmd, domipaddress, REMOTE_LOGIN, REMOTE_PASSWORD)
-    subprocess.check_call('net usershare delete pg-tests-reports', shell=True)
+    subprocess.check_call('net usershare delete pg-tests-reports',
+                          shell=True)
 
 
-def exec_command(cmd, hostname, login, password, skip_ret_code_check=False, connect_retry_count=3):
+def exec_command(cmd, hostname, login, password,
+                 skip_ret_code_check=False, connect_retry_count=3):
 
     import paramiko
 
@@ -139,7 +142,9 @@ def exec_command(cmd, hostname, login, password, skip_ret_code_check=False, conn
             client = paramiko.SSHClient()
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=hostname, username=login, password=password, port=SSH_PORT, look_for_keys=False,
+            client.connect(hostname=hostname,
+                           username=login, password=password,
+                           port=SSH_PORT, look_for_keys=False,
                            timeout=CONNECT_RETRY_DELAY)
             chan = client.get_transport().open_session()
             break
@@ -179,7 +184,9 @@ def exec_command(cmd, hostname, login, password, skip_ret_code_check=False, conn
             return retcode, stdout, stderr
 
 
-def exec_command_win(cmd, hostname, user, password, skip_ret_code_check=False, connect_retry_count=3):
+def exec_command_win(cmd, hostname,
+                     user, password,
+                     skip_ret_code_check=False, connect_retry_count=3):
     """ Execute command on windows remote host
 
     :param cmd:
@@ -194,7 +201,8 @@ def exec_command_win(cmd, hostname, user, password, skip_ret_code_check=False, c
     for trc in range(connect_retry_count):
         try:
             p = winrm.Protocol(
-                endpoint='http://' + hostname + ':5985/wsman', transport='plaintext',
+                endpoint='http://' + hostname + ':5985/wsman',
+                transport='plaintext',
                 username=user,
                 password=password)
             shell_id = p.open_shell()
@@ -269,13 +277,18 @@ def get_distro(remote=False, ip=None):
         host_info = {}
         while len(host_info) == 0:
             host_info = get_os_type(ip)
-        return host_info['NAME'].strip('"'), host_info['VERSION_ID'].strip('"'), None
+        return host_info['NAME'].strip('"'), \
+            host_info['VERSION_ID'].strip('"'), None
     else:
         os = platform.platform()
         if "Linux" in os:
-            return platform.linux_distribution()[0].strip('"'), platform.linux_distribution()[1], platform.machine()
+            return platform.linux_distribution()[0].strip('"'), \
+                platform.linux_distribution()[1], \
+                platform.machine()
         elif "Windows" in os:
-            return platform.win32_ver()[0], platform.win32_ver()[1], platform.machine()
+            return platform.win32_ver()[0], \
+                platform.win32_ver()[1], \
+                platform.machine()
         else:
             raise Exception("Unknown OS platform (%s)." % os)
 
@@ -287,7 +300,8 @@ def get_os_type(ip):
     :return:
     """
     cmd = 'cat /etc/*-release'
-    retcode, stdout, stderr = exec_command(cmd, ip, REMOTE_ROOT, REMOTE_ROOT_PASSWORD)
+    retcode, stdout, stderr = exec_command(cmd, ip,
+                                           REMOTE_ROOT, REMOTE_ROOT_PASSWORD)
     if retcode == 0:
         return dict(
             v.split("=") for v in stdout.replace(
@@ -319,7 +333,8 @@ def create_env_info_from_config(env_name, config):
     for node in config_file.sections():
         for value in config_file.items(node):
             if value[0] == 'ip_address':
-                env_info[env_name]['nodes'].append({"domname": node, "ip": value[1]})
+                env_info[env_name]['nodes'].append(
+                    {"domname": node, "ip": value[1]})
     return env_info
 
 
