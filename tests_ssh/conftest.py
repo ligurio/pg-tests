3babdbe442e53a1d2eab5f2537086858a151fb29
Alexander Lakhin <a.lakhin@postgrespro.ru>
2017-12-22 18:22:58 +0300

    Wrap long lines, fix formatting (tests_ssh/conftest.py)

--- tests_ssh/conftest.py
+++ tests_ssh/conftest.py
@@ -20,21 +20,30 @@ def pytest_addoption(parser):
     :param parser pytest default param for command line args:
     :return:
     """
-    parser.addoption("--target", dest="target", help='system(s) under test (image(s))')
-    parser.addoption("--product_version", action="store", default='9.6',
-                     help="Specify product version. Available values: 9.5, 9.6")
-    parser.addoption("--product_name", action="store", default='postgrespro',
-                     help="Specify product name. Available values: postgrespro, postresql")
-    parser.addoption("--product_edition", action="store", default='ee',
-                     help="Specify product edition. Available values: ee, standard")
-    parser.addoption("--product_milestone", action="store", default='beta',
-                     help="Specify product milestone. Available values: beta")
-    parser.addoption("--branch", action="store",
-                     help="Specify branch")
-    parser.addoption("--sqlsmith-queries", action="store", default=10000,
-                     help="Number of sqlsmith queries.")
-    parser.addoption("--config", dest="config", action="store",
-                     help="Path to config file")
+    parser.addoption(
+        "--target", dest="target",
+        help='system(s) under test (image(s))')
+    parser.addoption(
+        "--product_version", action="store", default='9.6',
+        help="Specify product version. Available values: 9.5, 9.6")
+    parser.addoption(
+        "--product_name", action="store", default='postgrespro',
+        help="Specify product name. Available values: postgrespro, postresql")
+    parser.addoption(
+        "--product_edition", action="store", default='ee',
+        help="Specify product edition. Available values: ee, standard")
+    parser.addoption(
+        "--product_milestone", action="store", default='beta',
+        help="Specify product milestone. Available values: beta")
+    parser.addoption(
+        "--branch", action="store",
+        help="Specify branch")
+    parser.addoption(
+        "--sqlsmith-queries", action="store", default=10000,
+        help="Number of sqlsmith queries.")
+    parser.addoption(
+        "--config", dest="config", action="store",
+        help="Path to config file")
     parser.addoption("--skip_install", action="store_true")
 
 
@@ -52,7 +61,8 @@ def create_environment(request):
         environment.create_environment()
         yield environment
         if request.config.getoption('--config'):
-            print("Cluster was deployed from config. No teardown actions for this type of cluster deploy")
+            print("Cluster was deployed from config." \
+                " No teardown actions for this type of cluster deploy")
         else:
             environment.delete_env()
 
@@ -68,36 +78,42 @@ def install_postgres(request, create_environment):
     """
     from helpers.pginstance import PgInstance
     if request.config.getoption('--config'):
-        environment_info = create_env_info_from_config(request.node.name, request.config.getoption('--config'))
+        environment_info = create_env_info_from_config(
+            request.node.name, request.config.getoption('--config'))
         cluster_name = request.node.name
     else:
         environment_info = create_environment.env_info
-        cluster_name = "%s_%s" % (request.node.name, request.config.getoption('--target'))
+        cluster_name = "%s_%s" % (
+            request.node.name, request.config.getoption('--target'))
     cluster_nodes = []
     for node in environment_info[cluster_name]['nodes']:
-        cluster_node = PgInstance(version=request.config.getoption('--product_version'),
-                                  milestone=request.config.getoption('--product_milestone'),
-                                  name=request.config.getoption('--product_name'),
-                                  edition=request.config.getoption('--product_edition'),
-                                  branch=request.config.getoption('--branch'),
-                                  skip_install=False,
-                                  node_ip=node['ip'],
-                                  cluster=True)
-        cluster_node.install_product_cluster(version=request.config.getoption('--product_version'),
-                                             milestone=request.config.getoption('--product_milestone'),
-                                             name=request.config.getoption('--product_name'),
-                                             edition=request.config.getoption('--product_edition'),
-                                             branch=request.config.getoption('--branch'),
-                                             node_ip=node['ip'])
+        cluster_node = PgInstance(
+            version=request.config.getoption('--product_version'),
+            milestone=request.config.getoption('--product_milestone'),
+            name=request.config.getoption('--product_name'),
+            edition=request.config.getoption('--product_edition'),
+            branch=request.config.getoption('--branch'),
+            skip_install=False,
+            node_ip=node['ip'],
+            cluster=True)
+        cluster_node.install_product_cluster(
+            version=request.config.getoption('--product_version'),
+            milestone=request.config.getoption('--product_milestone'),
+            name=request.config.getoption('--product_name'),
+            edition=request.config.getoption('--product_edition'),
+            branch=request.config.getoption('--branch'),
+            node_ip=node['ip'])
         cluster_nodes.append(cluster_node)
     yield cluster_nodes
     if request.config.getoption('--config'):
-        print("Cluster was deployed from config. No teardown actions for this type of cluster deploy")
+        print("Cluster was deployed from config."
+            " No teardown actions for this type of cluster deploy")
     else:
         create_environment.delete_env()
 
 # TODO create different behaviour for multimaster and replica,
-# TODO example here: https://docs.pytest.org/en/latest/proposals/parametrize_with_fixtures.html
+# TODO example here: https://docs.pytest.org/en/latest/
+#                     proposals/parametrize_with_fixtures.html
 # @pytest.fixture(params=['multimaster', 'replica'])
 # def deploy_cluster(request):
 #     if request.param == 'multimaster':
