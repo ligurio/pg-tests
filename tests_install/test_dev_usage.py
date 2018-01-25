b58eafede86ad3f86ba88958e6a815d2490b8b11
Alexander Lakhin <a.lakhin@postgrespro.ru>
2018-01-25 09:50:39 +0300

    Adjust test_dev_usage for Ubuntu 17.10

--- tests_install/test_dev_usage.py
+++ tests_install/test_dev_usage.py
@@ -76,7 +76,8 @@ set -e
 if which apt-get; then
     apt-get install -y gcc || true
     apt-get install -y make
-    grep 'Debian GNU/Linux 9' /etc/os-release && apt install -y libdpkg-perl
+    grep -E '(Debian GNU/Linux 9|"Ubuntu 17.10")' /etc/os-release && \
+        apt install -y libdpkg-perl
 elif which zypper; then
     zypper install -y gcc make
 elif which yum; then
