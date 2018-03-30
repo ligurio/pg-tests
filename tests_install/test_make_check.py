6831253f6c0cd053d519720e1ae0adce7cf2c986
Alexander Lakhin <a.lakhin@postgrespro.ru>
2018-03-30 16:39:47 +0300

    Load pgaudit to make check PROEE10_cert

--- tests_install/test_make_check.py
+++ tests_install/test_make_check.py
@@ -26,7 +26,7 @@ PRELOAD_LIBRARIES = {
          'pgpro_scheduler', 'pg_stat_statements', 'plantuner',
          'shared_ispell', 'pg_wait_sampling', 'pg_pathman'],
     'cert-enterprise-10':
-        ['auth_delay', 'auto_explain',
+        ['auth_delay', 'auto_explain', 'pgaudit',
          'pgpro_scheduler', 'pg_stat_statements', 'plantuner',
          'shared_ispell', 'pg_wait_sampling', 'pg_shardman',
          'pg_pathman'],
