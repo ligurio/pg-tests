c2327f6b84a43781c7a875391bb4b0cd4ca3a780
Alexander Lakhin <a.lakhin@postgrespro.ru>
2018-10-17 21:00:04 +0300

    Fix libecpg library name for 9.6 on Debian

--- helpers/pginstall.py
+++ helpers/pginstall.py
@@ -189,7 +189,7 @@ class PgInstall:
                     return self.get_base_package_name() + '*' + \
                         ' %s-.*-%s' % (self.product,
                                        self.version.replace('.', '\\.')) + \
-                        ' postgrespro-libecpg*'
+                        ' libecpg.*'
         return self.get_base_package_name() + '*'
 
     def __is_os_redhat_based(self):
