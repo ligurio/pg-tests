b27c20fc1b3422e7dce34cb4c16354e708508801
Alexander Lakhin <a.lakhin@postgrespro.ru>
2019-03-04 15:38:26 +0300

    Remove a workaround for PGPRO-2322

--- helpers/pginstall.py
+++ helpers/pginstall.py
@@ -779,10 +779,6 @@ baseurl=%s
     def install_full_topless(self):
         self.setup_extra_repos()
         pkgs = self.all_packages_in_repo
-        # pgbadger, pg-repack (PGPRO-2322)
-        for pkg in pkgs[:]:
-            if ('pgbadger' in pkg) or ('repack' in pkg):
-                pkgs.remove(pkg)
         if self.product == 'postgrespro' and \
                 self.version not in ['9.5', '9.6'] and \
                 self.get_base_package_name():
