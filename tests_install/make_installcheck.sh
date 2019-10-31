954185b5e678bec21f9dbf70095e682d12f77d89
Alexander Lakhin <a.lakhin@postgrespro.ru>
2019-10-31 12:37:37 +0300

    Fix max_replication_slots for installcheck with version 12

--- tests_install/make_installcheck.sh
+++ tests_install/make_installcheck.sh
@@ -134,7 +134,7 @@ if [ $exitcode -eq 0 ]; then
 port=25432\n
 autovacuum=off\n
 old_snapshot_threshold=0\n
-max_replication_slots=10\n
+max_replication_slots=4\n
 wal_level=logical\n
 max_prepared_transactions=10\n" >> tmpdb/postgresql.conf
     sudo -u postgres $1/bin/pg_ctl -D tmpdb -l tmpdb.log -w start
