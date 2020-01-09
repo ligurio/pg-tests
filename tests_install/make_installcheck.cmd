ae65b9c0f5afe92a72f7f58604ceeb8e339839b4
Alexander Lakhin <a.lakhin@postgrespro.ru>
2020-01-09 14:01:26 +0300

    Rid of requirement for Date::Parse perl package (https://git.postgrespro.ru/pgpro-dev/postgrespro/commit/890ce655)

--- tests_install/make_installcheck.cmd
+++ tests_install/make_installcheck.cmd
@@ -65,9 +65,6 @@ cd /var/src
 curl -s -O http://cpan.metacpan.org/authors/id/T/TO/TODDR/IPC-Run-0.96.tar.gz
 tar fax IPC-Run*
 (cd IPC-Run*/ && perl Makefile.PL && make && make install)
-curl -s -O http://cpan.metacpan.org/authors/id/G/GB/GBARR/TimeDate-2.30.tar.gz
-tar fax TimeDate*
-(cd TimeDate*/ && perl Makefile.PL && make && make install)
 echo "`date -Iseconds`: Source archive extracting... "
 tar fax postgres*.tar.bz2
 cd postgres*/
