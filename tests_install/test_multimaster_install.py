7b8bd7958a2ac9df2efa4a130a7b0defa86dc54d
Alexey Masterov <a.masterov@postgrespro.ru>
2020-03-02 17:50:22 +0700

    Fix errors

--- tests_install/test_multimaster_install.py
+++ tests_install/test_multimaster_install.py
@@ -189,7 +189,8 @@ class Multimaster(object):
             nodes[i].psql(
                 "CREATE DATABASE %s OWNER %s" % (
                     self.db, self.dbuser))
-            if platform.system == 'Windows':
+            if platform.system() == 'Windows':
+                print('Adding log settings for Windows')
                 nodes[i].add_config("log_destination = 'stderr'",
                                     "logging_collector = 'on'",
                                     "log_directory = 'log'")
