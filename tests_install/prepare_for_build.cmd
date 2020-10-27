2b6f9985aa47c0281162b01d4633f73569995ee9
Alexander Lakhin <a.lakhin@postgrespro.ru>
2020-10-27 07:28:33 +0300

    Switch prepare_for_build.cmd to a newer msys

--- tests_install/prepare_for_build.cmd
+++ tests_install/prepare_for_build.cmd
@@ -10,7 +10,7 @@ SET PGPATH=%2
 powershell -Command "((new-object net.webclient).DownloadFile('https://netcologne.dl.sourceforge.net/project/sevenzip/7-Zip/9.20/7za920.zip', '%TEMP%\7z.zip'))"
 powershell -Command "$shell = New-Object -ComObject Shell.Application; $zip_src = $shell.NameSpace('%TEMP%\7z.zip'); $zip_dest = $shell.NameSpace('%TEMP%'); $zip_dest.CopyHere($zip_src.Items(), 1044)"
 
-powershell -Command "((new-object net.webclient).DownloadFile('http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20180531.tar.xz', '%TEMP%\msys.tar.xz'))"
+powershell -Command "((new-object net.webclient).DownloadFile('http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20200903.tar.xz', '%TEMP%\msys.tar.xz'))"
 %TEMP%\7za.exe x %TEMP%\msys.tar.xz -so 2>%TEMP%/7z-msys0.log | %TEMP%\7za.exe x -aoa -si -ttar >%TEMP%/7z-msys1.log 2>&1
 
 REM First run is performed to setup the environment
