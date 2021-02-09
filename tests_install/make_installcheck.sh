bcf8c1455e13fbb5bb19e8511ca7db3b4c164923
Alexander Lakhin <a.lakhin@postgrespro.ru>
2021-02-09 17:36:11 +0300

    Temporarily disable a pair of installcheck tests due to PGPRO-4563

--- tests_install/make_installcheck.sh
+++ tests_install/make_installcheck.sh
@@ -189,10 +189,10 @@ psql -c 'SHOW data_directory';
 make installcheck -C src/interfaces/libpq &&
 make installcheck -C src/test/modules/commit_ts &&
 make installcheck -C src/test/modules/test_pg_dump &&
-make installcheck-force -C src/test/modules/snapshot_too_old &&
+echo PGPRO-4563 Disabled: make installcheck-force -C src/test/modules/snapshot_too_old &&
 if [ -d src/test/modules/brin ]; then make installcheck -C src/test/modules/brin; fi &&
 if [ -d src/test/modules/unsafe_tests ]; then make installcheck -C src/test/modules/unsafe_tests; fi &&
-make installcheck-force -C contrib/test_decoding"
+echo PGPRO-4563 Disabled: make installcheck-force -C contrib/test_decoding"
     exitcode=$?
     sudo -u postgres $1/bin/pg_ctl -D tmpdb -w stop
 fi
