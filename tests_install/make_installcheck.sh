8f292f65738ad30019bc251ac605b537243261c4
Denis Frolov <d.frolov@postgrespro.ru>
2021-02-18 11:24:49 +0300

    Small Fix3  in tests_install/make_installcheck.sh

--- tests_install/make_installcheck.sh
+++ tests_install/make_installcheck.sh
@@ -164,7 +164,7 @@ if [ $exitcode -eq 0 ]; then
             service "$2" restart
         fi
         if [ $comp == pgpro_controldata ]; then
-        $EXTRAVARS="enable_tap_tests=yes PROVE=\"PG_REGRESS=$1/lib/pgxs/src/test/regress/pg_regress prove\" PROVE_FLAGS=\"-I $BASEDIR/src/test/perl\""
+            EXTRAVARS="enable_tap_tests=yes PROVE=\"PG_REGRESS=$1/lib/pgxs/src/test/regress/pg_regress prove\" PROVE_FLAGS=\"-I $BASEDIR/src/test/perl\""
         fi
         echo "Performing 'make installcheck' for $comp..."
         tar fax $comp*.tar* &&
